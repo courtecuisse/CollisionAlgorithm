661cece290581be0b3c9599e52cb2b6cca44c0aa
hadrien <hadrien.courtecuisse@gmail.com>
2019-03-14 14:18:20 +0100

    "add missing file"

---
+++ src/sofa/collisionAlgorithm/BaseAlgorithm.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <sofa/collisionAlgorithm/BaseGeometry.h>
+#include <sofa/collisionAlgorithm/proximity/FixedProximity.h>
+#include <sofa/core/collision/Pipeline.h>
+
+namespace sofa
+{
+
+namespace collisionAlgorithm
+{
+
+class BaseFilter : public sofa::core::objectmodel::BaseObject {
+public:
+    SOFA_ABSTRACT_CLASS(BaseFilter, sofa::core::objectmodel::BaseObject);
+
+    virtual bool accept(const BaseProximity::SPtr & p1,const BaseProximity::SPtr & p2) const = 0;
+};
+
+typedef std::pair<BaseProximity::SPtr,BaseProximity::SPtr> PairDetection;
+
+class BaseAlgorithm : public core::objectmodel::BaseObject
+{
+public :
+
+    SOFA_ABSTRACT_CLASS(BaseAlgorithm, core::objectmodel::BaseObject);
+
+    BaseAlgorithm()
+    : l_filters(initLink("filters","list of filters")) {}
+
+    bool acceptFilter(const BaseProximity::SPtr & pfrom,const BaseProximity::SPtr & pdest) const {
+        for (auto itfilter = l_filters.begin();itfilter != l_filters.end();itfilter++) {
+            const BaseFilter * filter = (*itfilter);
+            if (filter == NULL) continue;
+            if (! filter->accept(pdest,pfrom)) return false;
+        }
+        return true;
+    }
+
+    virtual void processAlgorithm(BaseElementIterator::UPtr it, const BaseGeometry * g2, helper::vector< PairDetection > & output) = 0;
+
+protected:
+    core::objectmodel::MultiLink<BaseAlgorithm,BaseFilter,BaseLink::FLAG_STRONGLINK|BaseLink::FLAG_STOREPATH> l_filters;
+
+};
+
+}
+
+}
