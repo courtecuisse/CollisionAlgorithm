d3e7eadc42be7788c7a939123a2a45b32f4b4df2
Paul Baksic <p.baksic@unistra.fr>
2020-07-09 09:58:45 +0200

    Make AABB methods as toolbox methods.

---
+++ src/sofa/collisionAlgorithm/toolBox/AABBMethods.cpp
@@ -0,0 +1,186 @@
+#include <sofa/collisionAlgorithm/toolBox/ClosestProximityAlgorithm.h>
+#include <sofa/collisionAlgorithm/toolBox/AABBMethods.h>
+#include <sofa/core/ObjectFactory.h>
+#include <sofa/collisionAlgorithm/geometry/SubsetGeometry.h>
+
+namespace sofa {
+
+namespace collisionAlgorithm {
+
+namespace toolBox {
+void fillElementSet(const BaseGeometry::BroadPhase::SPtr decorator, defaulttype::Vec3i cbox, std::set<unsigned> & selectElements, int d) {
+    defaulttype::Vec3i nbox = decorator->getBoxSize();
+
+    {
+        int i=-d;
+        if (cbox[0]+i >= 0 && cbox[0]+i < nbox[0])
+        {
+            for (int j=-d; j <= d; j++)
+            {
+                if (cbox[1]+j < 0 || cbox[1]+j >= nbox[1])
+                    continue;
+                for (int k=-d;k<=d;k++)
+                {
+                    if (cbox[2]+k < 0 || cbox[2]+k >= nbox[2])
+                        continue;
+
+                    decorator->getElementSet(defaulttype::Vec3i(cbox[0] + i,cbox[1] + j,cbox[2] + k), selectElements);
+                }
+            }
+        }
+    }
+
+    {
+        int i=d;
+        if (cbox[0]+i >= 0 && cbox[0]+i < nbox[0])
+        {
+            for (int j=-d;j<=d;j++)
+            {
+                if (cbox[1]+j < 0 || cbox[1]+j >= nbox[1])
+                    continue;
+
+                for (int k=-d;k<=d;k++)
+                {
+                    if (cbox[2]+k < 0 || cbox[2]+k >= nbox[2])
+                        continue;
+
+                    decorator->getElementSet(defaulttype::Vec3i(cbox[0] + i,cbox[1] + j,cbox[2] + k), selectElements);
+                }
+            }
+        }
+    }
+
+
+    {
+        int j=-d;
+        if (cbox[1]+j >= 0 && cbox[1]+j < nbox[1])
+        {
+            for (int i=-d+1;i<d;i++)
+            {
+                if (cbox[0]+i < 0 || cbox[0]+i >= nbox[0])
+                    continue;
+
+                for (int k=-d;k<=d;k++)
+                {
+                    if (cbox[2]+k < 0 || cbox[2]+k >= nbox[2])
+                        continue;
+
+                    decorator->getElementSet(defaulttype::Vec3i(cbox[0] + i,cbox[1] + j,cbox[2] + k), selectElements);
+                }
+            }
+        }
+    }
+
+    {
+        int j=d;
+        if (cbox[1]+j >= 0 && cbox[1]+j < nbox[1])
+        {
+            for (int i=-d+1;i<d;i++)
+            {
+                if (cbox[0]+i < 0 || cbox[0]+i >= nbox[0])
+                    continue;
+
+                for (int k=-d;k<=d;k++)
+                {
+                    if (cbox[2]+k < 0 || cbox[2]+k >= nbox[2])
+                        continue;
+
+                    decorator->getElementSet(defaulttype::Vec3i(cbox[0] + i,cbox[1] + j,cbox[2] + k), selectElements);
+                }
+            }
+        }
+    }
+
+    {
+        int k=-d;
+        if (cbox[2]+k >= 0 && cbox[2]+k < nbox[2])
+        {
+            for (int i=-d+1;i<d;i++)
+            {
+                if (cbox[0]+i < 0 || cbox[0]+i >= nbox[0])
+                    continue;
+
+                for (int j=-d+1;j<d;j++)
+                {
+                    if (cbox[1]+j < 0 || cbox[1]+j >= nbox[1])
+                        continue;
+
+                    decorator->getElementSet(defaulttype::Vec3i(cbox[0] + i,cbox[1] + j,cbox[2] + k), selectElements);
+                }
+            }
+        }
+    }
+
+    {
+        int k=d;
+        if (cbox[2]+k >= 0 && cbox[2]+k < nbox[2])
+        {
+            for (int i=-d+1;i<d;i++)
+            {
+                if (cbox[0]+i < 0 || cbox[0]+i >= nbox[0])
+                    continue;
+
+                for (int j=-d+1;j<d;j++)
+                {
+                    if (cbox[1]+j < 0 || cbox[1]+j >= nbox[1])
+                        continue;
+
+                    decorator->getElementSet(defaulttype::Vec3i(cbox[0] + i,cbox[1] + j,cbox[2] + k), selectElements);
+                }
+            }
+        }
+    }
+}
+
+BaseProximity::SPtr findClosestProximity(const BaseProximity::SPtr & pfrom, BaseGeometry *geo,
+                                         std::function<bool(const collisionAlgorithm::PairDetection & )> acceptFilter,
+                                         std::function<double(const collisionAlgorithm::PairDetection & )> distance )
+{
+    BaseGeometry::BroadPhase::SPtr decorator = geo->getBroadPhase();
+
+    if (decorator == NULL) {
+        BaseElementIterator::UPtr begin = geo->begin();
+        return toolBox::doFindClosestProximityIt(pfrom,begin,
+                                                 acceptFilter,
+                                                 distance);
+    } else {
+        //take the first broad phase...
+        defaulttype::Vector3 P = pfrom->getPosition();
+
+        defaulttype::Vec3i bindex = decorator->getBoxCoord(P);
+        defaulttype::Vec3i bsize = decorator->getBoxSize();
+
+        int max = 0;
+
+        for (int i = 0 ; i < 3 ; i++) {
+            max = std::max (max, bindex[i]) ;
+            max = std::max (max, bsize[i]-bindex[i]) ;
+        }
+
+        BaseProximity::SPtr minprox_dest = nullptr;
+        int d = 0;
+        std::set<unsigned> selectedElements;
+        while (selectedElements.empty() && d<max) {
+            fillElementSet(decorator,bindex,selectedElements,d);
+
+            BaseElementIterator::UPtr begin(new SubsetElementIterator(geo, selectedElements));
+            minprox_dest = toolBox::doFindClosestProximityIt(pfrom, begin,
+                                                             acceptFilter,
+                                                             distance);
+
+            d++;// we look for boxed located at d+1
+
+            //take the first on that satisfy filters
+            if (minprox_dest != NULL) return minprox_dest;
+        }
+    }
+
+    return NULL;
+}
+
+}
+
+}
+
+}
+
---
+++ src/sofa/collisionAlgorithm/toolBox/AABBMethods.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <sofa/collisionAlgorithm/BaseProximity.h>
+#include <sofa/collisionAlgorithm/BaseGeometry.h>
+#include <sofa/collisionAlgorithm/BaseAlgorithm.h>
+#include <sofa/collisionAlgorithm/toolBox/ClosestProximityAlgorithm.h>
+
+namespace sofa {
+
+namespace collisionAlgorithm {
+
+namespace toolBox {
+
+void fillElementSet(const BaseGeometry::BroadPhase::SPtr decorator, defaulttype::Vec3i cbox, std::set<unsigned> & selectElements, int d) ;
+
+BaseProximity::SPtr findClosestProximity(const BaseProximity::SPtr & pfrom, BaseGeometry *geo,
+                                         std::function<bool(const collisionAlgorithm::PairDetection & )> acceptFilter = noFilter,
+                                         std::function<double(const collisionAlgorithm::PairDetection & )> distance = distance3d);
+
+}
+
+} // namespace component
+
+} // namespace sofa
+
