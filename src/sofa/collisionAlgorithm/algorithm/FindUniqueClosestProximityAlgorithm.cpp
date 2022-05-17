1442dabd42e78cb862f12e5415cf487329f29b99
Paul Baksic <p.baksic@unistra.fr>
2022-05-17 15:45:14 +0200

    ADD FindUniqueClosestProximityAlgorithm

---
+++ src/sofa/collisionAlgorithm/algorithm/FindUniqueClosestProximityAlgorithm.cpp
@@ -0,0 +1,18 @@
+#include <sofa/collisionAlgorithm/algorithm/FindUniqueClosestProximityAlgorithm.h>
+#include <sofa/core/ObjectFactory.h>
+
+namespace sofa {
+
+namespace collisionAlgorithm {
+
+SOFA_DECL_CLASS(FindUniqueClosestProximityAlgorithm)
+
+int FindUniqueClosestProximityAlgorithmClass = core::RegisterObject("FindUniqueClosestProximityAlgorithm")
+.add< FindUniqueClosestProximityAlgorithm >();
+
+}
+
+}
+
+
+
